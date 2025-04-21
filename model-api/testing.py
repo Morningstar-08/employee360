787cc34d3dd2835c9406b098d5e7982c3a537ca0
toril jain <toriljain@gmail.com>
2025-04-21 13:57:05 +0530

    xgboost classification

--- model-api/testing.py
+++ model-api/testing.py
@@ -4,7 +4,7 @@ import shap
 import numpy as np
 
 # Load trained artifacts
-model = joblib.load("model-api/employee_retention_model.pkl")
+model = joblib.load("model-api/employee_retention_model_classification.pkl")
 scaler = joblib.load("model-api/scaler.pkl")
 ohe = joblib.load("model-api/ohe.pkl")
 columns = joblib.load("model-api/feature_names.pkl")
@@ -13,20 +13,20 @@ feature_map = joblib.load("model-api/feature_map.pkl")
 cat_cols = ['OverTime', 'Gender', 'Department', 'MaritalStatus', 'JobRole']
 
 query = {
-    "Age": 95,
-    "Education": 3,
-    "EnvironmentSatisfaction": 4,
-    "JobInvolvement": 3,
-    "JobLevel": 2,
-    "JobSatisfaction": 4,
-    "MonthlyIncome": 5000,
-    "PercentSalaryHike": 12,
-    "PerformanceRating": 3,
-    "StockOptionLevel": 1,
-    "WorkLifeBalance": 2,
+    "Age": 30,
+    "Education": 4,
+    "EnvironmentSatisfaction": 1,
+    "JobInvolvement": 1,
+    "JobLevel": 4,
+    "JobSatisfaction": 1,
+    "MonthlyIncome": 50000,
+    "PercentSalaryHike": 15,
+    "PerformanceRating": 4,
+    "StockOptionLevel": 3,
+    "WorkLifeBalance": 4,
     "YearsAtCompany": 5,
     "YearsSinceLastPromotion": 1,
-    "OverTime": "Yes",
+    "OverTime": "No",
     "Gender": "Female",
     "Department": "Research & Development",
     "MaritalStatus": "Married",
@@ -57,9 +57,9 @@ df_unscaled = df.copy()
 df_scaled = scaler.transform(df)
 
 # Step 6: Prediction
-# prob = model.predict(df_scaled)[0]
-prob = np.clip(model.predict(df_scaled)[0], 0, 1)
-
+prob = model.predict_proba(df_scaled)[0][1]  # Get probability of class 1 (attrition)
+# prob = np.clip(model.predict(df_scaled)[0], 0, 1)
+print(prob)
 if prob <= 0.4:
     risk = "low_risk"
 elif prob <= 0.7:
@@ -87,9 +87,8 @@ def explain_employee_summary(emp_id, X_unscaled, shap_values, feature_map, top_n
         if feature.startswith("Department_") or feature in feature_map:
             continue  # skip excluded features
         actual_value = row[feature]
-        median = X_unscaled[feature].median()
-        direction = "high_" if actual_value < median else "low"
-        reasons.append(f"{direction} {feature}")
+        median = X_unscaled[feature].mode()[0]
+        reasons.append(feature)
         count += 1
         if count >= top_n:
             break
